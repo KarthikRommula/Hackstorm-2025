910b5d7bb298678e24b2e4591c2c39973ce42499
KarthikRommula <officialkarthik1819@gmail.com>
2025-02-19 13:56:37 +0530

    update

--- src/components/HackathonWebsite.js
+++ src/components/HackathonWebsite.js
@@ -434,14 +434,16 @@ const tabCategories = {
                                         <br />
                                         <div className="mt-4">
                                             <iframe
-                                                src="https://maps.google.com/maps?q=17.385044,78.486671&z=15&output=embed"
+                                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2264.5937963021493!2d78.28989077328994!3d17.335917334148874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcbeab272796741%3A0x73864d0007766678!2sKG%20Reddy%20College%20of%20Engineering%20and%20Technology!5e0!3m2!1sen!2sin!4v1739953467651!5m2!1sen!2sin"
                                                 width="100%"
                                                 height="300"
                                                 style={{ border: 0, borderRadius: "10px" }}
                                                 allowFullScreen
                                                 loading="lazy"
+                                                referrerPolicy="no-referrer-when-downgrade"
                                             ></iframe>
                                         </div>
+
 
                                         {/* External Google Maps Link */}
                                         <a
